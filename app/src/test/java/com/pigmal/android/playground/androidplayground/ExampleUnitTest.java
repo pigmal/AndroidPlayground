869739dca8630d7eed22d91d8c8bf9b731859ee5
itog <itog@pigmal.com>
2016-01-06 13:46:46 +0900

    Create new project.

---
+++ app/src/test/java/com/pigmal/android/playground/androidplayground/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.pigmal.android.playground.androidplayground;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
