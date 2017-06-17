75816f4f60afc7dbd202470e2b8748a8b3a26d7f
itog <itog@pigmal.com>
2017-06-17 10:42:58 +0900

    Add a graph to show accelerometer value

--- app/src/main/java/com/pigmal/android/playground/androidplayground/accelerometer/AccelerometerActivity.java
+++ app/src/main/java/com/pigmal/android/playground/androidplayground/accelerometer/AccelerometerActivity.java
@@ -1,5 +1,6 @@
 package com.pigmal.android.playground.androidplayground.accelerometer;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,12 +10,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.pigmal.android.playground.androidplayground.R;
 
 public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
     private static final String TAG = "Accelerometer";
     SensorManager mSensorManager;
     Sensor mSensor;
+
+    LineChart mChart;
+
+    String[] mLabels = new String[]{"x", "y", "z"};
+    int[] mColors = new int[]{Color.RED, Color.GREEN, Color.BLUE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,9 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
 
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mChart = (LineChart) findViewById(R.id.lineChart);
+        mChart.setData(new LineData());
     }
 
     @Override
@@ -45,10 +59,39 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
             + String.valueOf(event.values[0]) + ", "
             + String.valueOf(event.values[1]) + ", "
             + String.valueOf(event.values[2]) + ")");
+
+        LineData data = mChart.getLineData();
+        if (data != null) {
+            for (int i = 0; i < 3; i++) {
+                ILineDataSet set = data.getDataSetByIndex(i);
+                if (set == null) {
+                    set = createSet(mLabels[i], mColors[i]);
+                    data.addDataSet(set);
+                }
+
+                data.addEntry(new Entry(set.getEntryCount(), event.values[i]), i);
+                data.notifyDataChanged();
+            }
+
+            mChart.notifyDataSetChanged();
+            mChart.setVisibleXRange(50, 50);
+            mChart.getXAxis().setDrawLabels(false);
+            mChart.moveViewToX(data.getEntryCount());
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    private LineDataSet createSet(String label, int color) {
+        LineDataSet set = new LineDataSet(null, label);
+        set.setLineWidth(2.5f);
+        set.setColor(color);
+        set.setDrawCircles(false);
+        set.setDrawValues(false);
+
+        return set;
     }
 }
